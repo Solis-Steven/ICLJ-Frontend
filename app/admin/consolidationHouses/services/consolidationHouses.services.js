5b5582db3fd8eee2b8c35b94be4c2af705d882f1
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2023-12-29 16:25:08 -0500

    Get and Delete consolidationHouse

---
+++ app/admin/consolidationHouses/services/consolidationHouses.services.js
@@ -0,0 +1,50 @@
+import { axiosClient } from "@/config/axiosClient";
+//Obtener todas las casas de consolidación
+export const getAllConsolidationHouses = async () => {
+  try {
+
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      return;
+    }
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    }
+    const { data } = await axiosClient("consolidationHouses", config);
+
+    return (data);
+  } catch (error) {
+    console.log("Error al obtener casas de consolidation")
+  }
+};
+export const deleteConsolidationHouseById = async (consolidationHouseId) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      throw new Error("Token not found");
+    }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    };
+
+    // Modifica la URL según tu implementación
+    const url = `consolidationHouses/${consolidationHouseId}`;
+
+    const response = await axiosClient.delete(url, config)
+
+    // Puedes retornar la respuesta si lo necesitas
+    return response.data;
+  } catch (error) {
+    console.error("Error al eliminar casa de consolidación:", error.message);
+    throw error;
+  }
+};
