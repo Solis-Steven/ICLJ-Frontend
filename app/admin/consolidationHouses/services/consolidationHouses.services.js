e0cba2ac4b27571a805994583c60ab44e2aa7b62
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2023-12-30 18:26:47 -0500

    Requisito completo del lado del admin
    creación y implementación del modal editar

--- app/admin/consolidationHouses/services/consolidationHouses.services.js
+++ app/admin/consolidationHouses/services/consolidationHouses.services.js
@@ -1,20 +1,10 @@
 import { axiosClient } from "@/config/axiosClient";
+import { getToken } from "@/utilities/getToken";
 //Obtener todas las casas de consolidación
 export const getAllConsolidationHouses = async () => {
   try {
 
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-      return;
-    }
-
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-      }
-    }
+    const config = getToken()
     const { data } = await axiosClient("consolidationHouses", config);
 
     return (data);
@@ -24,17 +14,7 @@ export const getAllConsolidationHouses = async () => {
 };
 export const deleteConsolidationHouseById = async (consolidationHouseId) => {
   try {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-      throw new Error("Token not found");
-    }
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-      }
-    };
+    const config = getToken()
 
     // Modifica la URL según tu implementación
     const url = `consolidationHouses/${consolidationHouseId}`;
@@ -50,28 +30,48 @@ export const deleteConsolidationHouseById = async (consolidationHouseId) => {
 };
 export const CreateConsolidationHouse = async ({ name, leader, date, address  }) => {
   try {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-      throw new Error("Token not found");
-    }
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-      }
-    };
-
+    const config = getToken()
     // Modifica la URL según tu implementación
-    
-   
-    const { data } = await axiosClient.post("/consolidationHouses", { name, leader, date, address  }, config);
+    const isoDate = new Date(date).toISOString();
+    const { data } = await axiosClient.post("/consolidationHouses", { name, leader, date: isoDate, address  }, config);
     // Puedes retornar la respuesta si lo necesitas
     return data;
 
     // Puedes retornar la respuesta si lo necesitas
   } catch (error) {
     console.error("Error al agregar casa de consolidación:", error.message);
+    throw error;
+  }
+};
+export const getConsolidationHouseById = async (consolidationHouseId) => {
+  try {
+    const config = getToken()
+
+    // Modifica la URL según tu implementación
+    const url = `consolidationHouses/${consolidationHouseId}`;
+
+    const response = await axiosClient.get(url, config)
+
+    // Puedes retornar la respuesta si lo necesitas
+    return response.data;
+  } catch (error) {
+    console.error(`Error al obtener la casa de consolidación ${consolidationHouseId}:`, error.message);
+    throw error;
+  }
+};
+export const UpdateConsolidationHouseById = async (consolidationHouseId,{ name, leader, date, address }) => {
+  try {
+    const config = getToken()
+
+    // Modifica la URL según tu implementación
+    const url = `consolidationHouses/${consolidationHouseId}`;
+    const isoDate = new Date(date).toISOString();
+    const response = await axiosClient.put(url, { name, leader, date: isoDate, address  }, config)
+
+    // Puedes retornar la respuesta si lo necesitas
+    return response.data;
+  } catch (error) {
+    console.error("Error al actualizar casa de consolidación:", error.message);
     throw error;
   }
 };
