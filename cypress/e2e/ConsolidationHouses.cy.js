58c6dddd45384235297d24d5cf054ae025344427
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2024-01-02 23:23:17 -0600

    pruebas casa de consolidación

--- cypress/e2e/ConsolidationHouses.cy.js
+++ cypress/e2e/ConsolidationHouses.cy.js
@@ -1,5 +1,4 @@
-
-describe("template spec", () => {
+describe("Casas de Consolidación", () => {
   beforeEach(() => {
     cy.visit("/login");
 
@@ -12,28 +11,123 @@ describe("template spec", () => {
     cy.url().should("include", "/admin/members");
   });
 
-  it("Crear casa de consolidación", () => {
+    it("Crear casa de consolidación Shekinah debe dar error", () => {
     cy.visit("/admin/consolidationHouses");
     cy.contains("Agregar CDC").click();
-    cy.get('input[placeholder="Ingrese el nombre de la CDC"]').type("hogar");
+    cy.get('input[placeholder="Ingrese el nombre de la CDC"]').type("Shekinah");
     // Selecciona el valor del líder en el dropdown
     cy.get("select#leader").select("Steven Solis Obando"); // Puedes ajustar el valor según tus necesidades
     // Rellena el input de Fecha
-    cy.get('input[type="date"]').type("2024-01-01"); // Puedes ajustar la fecha según tus necesidades
+    //cy.get('input[type="date"]').type("2024-01-01"); // Puedes ajustar la fecha según tus necesidades
     // Rellena el input de Hora
-    cy.get('input[type="time"]').type("12:00"); // Puedes ajustar la hora según tus necesidades
+    cy.get('input[type="time"]').type("23:45"); // Puedes ajustar la hora según tus necesidades
     // Rellena el textarea de Dirección
     cy.get('textarea[name="address"]').type(
       "500 metros norte del super casa de todos en barrio Lourdes"
     );
     // Haz clic en el botón "Guardar"
     //cy.contains("Guardar").click();
-    cy.xpath('/html/body/div[3]/div/div/div/div/div[2]/div[5]/button').click();
-    cy.contains("hogar").should("exist");
+    cy.xpath("/html/body/div[3]/div/div/div/div/div[2]/div[5]/button").click();
+    // Verifica que el mensaje de éxito esté presente en el elemento con role="status"
+    cy.wait(1000);
+    cy.get('[role="status"][aria-live="polite"]').should(
+      "contain",
+      "obligatorios"
+    );
+
+
   });
- 
-  
- 
+     it("Crear casa de consolidación Apostol", () => {
+    cy.visit("/admin/consolidationHouses");
+    cy.contains("Agregar CDC").click();
+    cy.get('input[placeholder="Ingrese el nombre de la CDC"]').type("Apostol");
+    // Selecciona el valor del líder en el dropdown
+    cy.get("select#leader").select("Luz"); // Puedes ajustar el valor según tus necesidades
+    // Rellena el input de Fecha
+    cy.get('input[type="date"]').type("2023-01-12"); // Puedes ajustar la fecha según tus necesidades
+    // Rellena el input de Hora
+    cy.get('input[type="time"]').type("23:45"); // Puedes ajustar la hora según tus necesidades
+    // Rellena el textarea de Dirección
+    cy.get('textarea[name="address"]').type(
+      "500 metros norte del super casa de todos en barrio Lourdes"
+    );
+    // Haz clic en el botón "Guardar"
+    cy.xpath("/html/body/div[3]/div/div/div/div/div[2]/div[5]/button").click();
+    // Verifica que el mensaje de éxito esté presente en el elemento con role="status"
+    cy.wait(1000);
+    cy.get('[role="status"][aria-live="polite"]').should(
+      "contain",
+      "creada exitosamente"
+    );
+  });
+  it("Editar casa de consolidación", () => {
+    cy.visit("/admin/consolidationHouses");
+    // Hacer clic en el botón "Editar" del primer elemento (index 0)
+    cy.get(
+      '.space-y-0.border.shadow-sm.border-inherit.border-border div.flex.flex-col.sm\\:flex-row button:contains("Editar")'
+    )
+      .eq(0)
+      .click();
+    cy.get('input[type="text"]').type("Venida de Dios");
+    // Selecciona el valor del líder en el dropdown
+    cy.get("select#leader").select("javier"); // Puedes ajustar el valor según tus necesidades
+    // Rellena el input de Fecha
+    cy.get('input[type="date"]').type("2023-01-12"); // Puedes ajustar la fecha según tus necesidades
+    // Rellena el input de Hora
+    cy.get('input[type="time"]').type("23:45"); // Puedes ajustar la hora según tus necesidades
+    // Rellena el textarea de Dirección
+    cy.get('textarea[name="address"]').type(
+      "50 mts este del parque"
+    );
+    cy.xpath("/html/body/div[3]/div/div/div/div/div[2]/div[5]/button").click();
+    cy.wait(1000);
+    cy.get('[role="status"][aria-live="polite"]').should(
+      "contain",
+      "editada exitosamente"
+    );
+  });
+  it("Editar casa de consolidación", () => {
+    cy.visit("/admin/consolidationHouses");
+    // Hacer clic en el botón "Editar" del primer elemento (index 0)
+    cy.get(
+      '.space-y-0.border.shadow-sm.border-inherit.border-border div.flex.flex-col.sm\\:flex-row button:contains("Editar")'
+    )
+      .eq(0)
+      .click();
+    // Rellena el input de Fecha
+    cy.get('input[type="date"]').type("2023-01-13"); // Puedes ajustar la fecha según tus necesidades
+    // Rellena el input de Hora
+    cy.get('input[type="time"]').type("23:45"); // Puedes ajustar la hora según tus necesidades
+    // Rellena el textarea de Dirección
+    cy.get('textarea[name="address"]').type(
+      "50 mts este del parque"
+    );
+    cy.xpath("/html/body/div[3]/div/div/div/div/div[2]/div[5]/button").click();
+    cy.wait(1000);
+    cy.get('[role="status"][aria-live="polite"]').should(
+      "contain",
+      "editada exitosamente"
+    );
+  });
+  it("Eliminar casa de consolidación", () => {
+    cy.visit("/admin/consolidationHouses");
+    // Hacer clic en el botón "Editar" del primer elemento (index 0)
+    cy.get(
+      '.space-y-0.border.shadow-sm.border-inherit.border-border div.flex.flex-col.sm\\:flex-row button:contains("Eliminar")'
+    )
+      .eq(0)
+      .click();
+    // Supongamos que el div tiene una clase única que lo identifica, por ejemplo, 'dialog-container'
+    cy.xpath("/html/body/div[3]/div/div/div/div/div[2]")
+      .find(".bg-red-600") // Asumiendo que la clase 'bg-red-600' identifica el botón Eliminar
+      .click();
+    cy.wait(1000);
+    cy.get('[role="status"][aria-live="polite"]').should(
+      "contain",
+      "eliminada exitosamente"
+    );
+  });
+
   afterEach(() => {
     cy.visit("/admin/members");
   });
