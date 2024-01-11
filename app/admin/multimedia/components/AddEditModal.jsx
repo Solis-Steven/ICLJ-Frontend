ab51b27e0b01b0cbc5df6a4e9b7c520277651ad2
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2024-01-11 13:59:40 -0600

    Cargar archivos y modal agregar
    multimedia

--- app/admin/multimedia/components/AddEditModal.jsx
+++ app/admin/multimedia/components/AddEditModal.jsx
@@ -1,30 +1,16 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState, useEffect } from "react";
 import { Input } from "@/components/Input";
-import { getAllMembers } from "../../members/services/member.services";
+import { Spinner } from "@/components/Spinner";
 export const AddEditModal = ({
-  EventFixedId,
+  multimediaId,
   isOpen,
   onClose,
   handleInputChange,
   handleSubmit,
   formData,
-  page,
-  isActive,
+  isLoading,
 }) => {
-  const [members, setMembers] = useState([]);
-
-  useEffect(() => {
-    const fetchMembers = async () => {
-      try {
-        const membersData = await getAllMembers({ page, isActive });
-        setMembers(membersData);
-      } catch (error) {
-        console.error("Error fetching members:", error);
-      }
-    };
-    fetchMembers();
-  }, []);
   return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog
@@ -85,76 +71,79 @@ export const AddEditModal = ({
                 as="h3"
                 className="text-lg leading-6 font-medium text-gray-900"
               >
-                {EventFixedId ? "Editar Evento Fijo" : "Crear Evento Fijo"}
+                {multimediaId ? "Editar multimedia" : "Crear multimedia"}
               </Dialog.Title>
 
               <Input
-                id="EventName"
+                id="MultimediaName"
                 labelText="Nombre"
-                placeholder="Nombre del evento fijo"
+                placeholder="Nombre del archivo"
                 type="text"
                 value={formData["name"]}
                 onChange={(value) => handleInputChange("name", value)}
               />
-              <label
-                htmlFor="leader"
-                className="uppercase block text-md font-bold text-gray-600"
-              >
-                ENCARGADO
-              </label>
-              <select
-                id="manager"
-                name="manager"
-                value={formData["manager"]}
-                onChange={(e) => handleInputChange("manager", e.target.value)}
-                className="mt-3 p-3 border rounded-xl bg-gray-50 w-full"
-              >
-                {members.map((miembro) => (
-                  <option key={miembro._id} value={miembro._id}>
-                    {miembro.name}
-                  </option>
-                ))}
-              </select>
-              <Input
-                id="date"
-                labelText="Fecha"
-                placeholder={"Ingrese la fecha"}
-                type="date"
-                value={formData["date"]}
-                onChange={(value) => handleInputChange("date", value)}
-              />
-              <Input
-                id="time}"
-                labelText="Hora"
-                placeholder="Ingrese la hora del CDC"
-                type="time"
-                value={formData["time"]}
-                onChange={(value) => handleInputChange("time", value)}
-              />
-              {/*  toggle for visible */}
               <div className=" flex items-center justify-between mr-4">
-              <span className="uppercase block text-md font-bold text-gray-600">
+                <span className="uppercase block text-md font-bold text-gray-600">
                   Visible
                 </span>
-              <label className="relative inline-flex items-center cursor-pointer">
-                <input
-                id="visible"
-                name="visible"
-                checked={formData["visible"]}
-                onChange={(e) => handleInputChange("visible", e.target.checked)}
-                 type="checkbox" className="sr-only peer" />
-                <div className=" w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600"></div>
-               
-              </label>
+                <label className="relative inline-flex items-center cursor-pointer">
+                  <input
+                    id="visible"
+                    name="visible"
+                    checked={formData["visible"]}
+                    onChange={(e) =>
+                      handleInputChange("visible", e.target.checked)
+                    }
+                    type="checkbox"
+                    className="sr-only peer"
+                  />
+                  <div className=" w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600"></div>
+                </label>
               </div>
-              
+              {/* Spinner*/}
+              {isLoading && <Spinner />}
+              <div className="mt-2 bg-white rounded-lg overflow-hidden flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.0"
+                  stroke="currentColor"
+                  class="w-20 h-20"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5 text-green-500 bg-white mt-12"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+                <span className="text-gray-500 text-sm font-semibold mt-12 ml-1">
+                    {"Completado"}
+                </span>
+              </div>
               <div className="mt-5 sm:mt-6">
                 <button
                   type="button"
-                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-secondary hover:bg-teal-300  text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                  className="inline-flex justify-center w-full rounded-md border border-transparent 
+                  shadow-sm px-4 py-2 bg-secondary hover:bg-teal-300  text-base font-medium text-white  focus:outline-none focus:ring-2 
+                  focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                   onClick={handleSubmit}
                 >
-                  Guardar
+                  {isLoading ? "Guardando..." : "Guardar"}
                 </button>
               </div>
             </div>
--- app/admin/multimedia/page.jsx
+++ app/admin/multimedia/page.jsx
@@ -1,11 +1,67 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import { UploadFiles } from "./components/UploadFiles";
+import { AddEditModal } from "./components/AddEditModal";
+import { notifyError } from "@/utilities/notifyError";
 
 const page = () => {
-    
-    return(
-        <>
-            <h1>Multimedia</h1>        
-        </>
-    );
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    ref: "",
+    visible: true,
+  });
+  const [multimediaId, setMultimediaId] = useState("");
+  const [originalMultimedia, setOriginalMultimedia] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [multimedia, setMultimedia] = useState([]);
+  const [file, setFile] = useState(null);
+  const handleFileChange = (e) => {
+    if(e.target.files[0].type === 'image/png' || e.target.files[0].type === 'image/jpg' || e.target.files[0].type === 'image/jpeg' || e.target.files[0].type === 'video/mp4'){
+      setFile(e.target.files[0]);
+      onClose();
+    }else{
+      notifyError('El formato del archivo no es valido');
+    }
+  }
+  const onClose = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleInputChange = (id, value) => {
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    onClose();
+  };
+
+  return (
+    <section className="w-full">
+      <h1 className="font-bold text-2xl mb-5">Multimedia</h1>
+      <section className="flex gap-3 items-center">
+        {/* button add */}
+        {/* Modal */}
+        <AddEditModal
+          multimediaId={multimediaId}
+          isOpen={isOpen}
+          onClose={onClose}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+          formData={formData}
+          isLoading={isLoading}
+        />
+        {/* search */}
+        <UploadFiles
+        handleFileChange={handleFileChange} />
+      </section>
+      <section className="shadow-lg p-5 mt-10">{/* lista */}</section>
+    </section>
+  );
+};
 
 export default page;
