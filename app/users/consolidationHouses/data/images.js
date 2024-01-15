1ab284e52ff644de136f98fb032925282d2660fd
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2024-01-15 16:31:36 -0600

    vista casas de consolidation actualizada

---
+++ app/users/consolidationHouses/data/images.js
@@ -0,0 +1,17 @@
+export const images = [
+	{
+		ref: "https://firebasestorage.googleapis.com/v0/b/iclj-7f0ec.appspot.com/o/banner%2Fbanner.png?alt=media&token=64d118ae-5b8c-4a48-b184-6dc80c223ca0",
+	},
+	{
+		ref: "https://firebasestorage.googleapis.com/v0/b/iclj-7f0ec.appspot.com/o/banner%2Fbanner.png?alt=media&token=64d118ae-5b8c-4a48-b184-6dc80c223ca0",
+	},
+	{
+		ref: "https://firebasestorage.googleapis.com/v0/b/iclj-7f0ec.appspot.com/o/banner%2Fbanner.png?alt=media&token=64d118ae-5b8c-4a48-b184-6dc80c223ca0",
+	},
+	{
+		ref: "https://firebasestorage.googleapis.com/v0/b/iclj-7f0ec.appspot.com/o/banner%2Fbanner.png?alt=media&token=64d118ae-5b8c-4a48-b184-6dc80c223ca0",
+	},
+	{
+		ref: "https://firebasestorage.googleapis.com/v0/b/iclj-7f0ec.appspot.com/o/banner%2Fbanner.png?alt=media&token=64d118ae-5b8c-4a48-b184-6dc80c223ca0",
+	},
+];
